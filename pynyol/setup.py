58229ec5d47d41b521b365e4c5f43292881f7774
Juanma López <lopezjuanma96@gmail.com>
2023-04-20 11:48:06 -0300

    hotfix for #6

--- pynyol/setup.py
+++ pynyol/setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='pynyol',
-    version='0.1.2',
+    version='0.1.3',
     author='Juanma López',
     author_email='lopezjuanma96@gmail.com',
     packages=['pynyol', 'pynyol.test'],
-    package_dir={'': './'},
+    package_dir={'': '.'},
     scripts=['bin/express_a_num.py'],
     url='https://pypi.org/project/pynyol',
     license='LICENSE.txt',
